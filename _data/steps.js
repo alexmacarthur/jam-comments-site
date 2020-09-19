a3ccba5094c8b0039e132cf7b6525469cc2a3552
Alex MacArthur <alex@macarthur.me>
2020-09-18 22:43:17 -0500

    wip - laying out the page.

---
+++ _data/steps.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        heading: "Create an Account",
+        description: "JamComments is FREE for a single static site and up to 10 comments per month."
+    },
+    {
+        heading: "Install & Configure the Plugin",
+        description: "Currently there's just a plugin for Gatsby. But soon enough, an easy-to-install plugin will be available for a range of static site generators."
+    }, 
+    {
+        heading: "Start Collecting Comments", 
+        description: "As new comments come in, your static site will be triggered to regenerate. So, you'll have the latest comments statically-rendered, ready to be read."
+    }
+];
