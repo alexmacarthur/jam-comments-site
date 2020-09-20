364b621d1d0fba2d7d3f2d5e86dbd9106764a9f5
Alex MacArthur <alex@macarthur.me>
2020-09-19 20:07:49 -0500

    fill out home page content.

---
+++ _data/homeHighlights.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        heading: "Your Jamstack blog isn't complicated. Comments shouldn't be either.",
+        description: "Current options to collect & display comments on your static site either means manually piecing together a hodge-podge of technologies, or submitting yourself to a behemoth, soulless service. JamComments doesn't try to be anything other than a stupid, simple comment service that's easy to set up and doesn't leave you feeling guilty after setting it up."
+    },
+    {
+        heading: "Not embedded. <em>Integrated.</em>",
+        description: "JamComments plugs directly into your SSG's build process (currently only <a href=\"https://www.gatsbyjs.com/\">GatsbyJS</a>, but more integrations coming soon!), spitting out statically rendered comments for your posts. Comments become an engagement-driving, performance-mindful <em>part</em> of your site's content -- not an afterthought."
+    },
+    {
+        heading: "No ads, sketchy data mining, or tracking.", 
+        description: "By being \"stupid-simple\", JamComments has no choice but to not give a rip about selling your readers garbage or collecting any bit of knowledge about them. It just collects & serves comments. Nothing fancy."
+    }
+];
