3d063cf3213c12236e19ce8569ebc7b729f28cd4
Alex MacArthur <alex@macarthur.me>
2020-09-20 00:05:14 -0500

    minify and stuff.

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,42 @@
 const { minify } = require("terser");
+const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addWatchTarget("./_tmp/style.css");
-  
-    eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
-  
-    eleventyConfig.addShortcode("version", function () {
-        return String(Date.now());
+  eleventyConfig.addWatchTarget("./_includes/style.css");
+
+  eleventyConfig.addShortcode("version", function () {
+      return String(Date.now());
+  });
+
+  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
+      code,
+      callback
+    ) {
+      try {
+        const minified = await minify(code);
+        callback(null, minified.code);
+      } catch (err) {
+        console.error("Terser error: ", err);
+        callback(null, code);
+      }
     });
 
-    eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
-        code,
-        callback
-      ) {
-        try {
-          const minified = await minify(code);
-          callback(null, minified.code);
-        } catch (err) {
-          console.error("Terser error: ", err);
-          callback(null, code);
-        }
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
       });
+
+      return minified;
+    }
+
+    return content;
+  });
 };
