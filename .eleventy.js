48a5998a7407becdd52220b4ae8209791a4b8a99
Alex MacArthur <alex@macarthur.me>
2020-09-19 21:22:16 -0500

    Setting up airtable-powered email form

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { minify } = require("terser");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./_tmp/style.css");
   
@@ -6,4 +8,17 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addShortcode("version", function () {
         return String(Date.now());
     });
+
+    eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
+        code,
+        callback
+      ) {
+        try {
+          const minified = await minify(code);
+          callback(null, minified.code);
+        } catch (err) {
+          console.error("Terser error: ", err);
+          callback(null, code);
+        }
+      });
 };
