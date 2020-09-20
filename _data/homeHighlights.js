7f8b33288fe03554d8e56907250a62d2c45dfbc0
Alex MacArthur <alex@macarthur.me>
2020-09-19 23:45:32 -0500

    lots more changes...

--- _data/homeHighlights.js
+++ _data/homeHighlights.js
@@ -1,14 +1,14 @@
 module.exports = [
     {
-        heading: "Your Jamstack blog isn't complicated. Comments shouldn't be either.",
-        description: "Current options to collect & display comments on your static site either means manually piecing together a hodge-podge of technologies, or submitting yourself to a behemoth, soulless service. JamComments doesn't try to be anything other than a stupid, simple comment service that's easy to set up and doesn't leave you feeling guilty after setting it up."
-    },
-    {
         heading: "Not embedded. Integrated.",
-        description: "JamComments plugs directly into your SSG's build process (currently only <a href=\"https://www.gatsbyjs.com/\">GatsbyJS</a>, but more integrations coming soon!), spitting out statically rendered comments for your posts. Comments become an engagement-driving, performance-mindful <em>part</em> of your site's content -- not an afterthought."
+        description: "JamComments will plug directly into your SSG's build process, spitting out statically rendered comments for your posts. Comments become an engagement-driving, performance-mindful <em>piece</em> of your site's content -- not an afterthought."
     },
     {
         heading: "No ads, sketchy data mining, or tracking.", 
-        description: "By being \"stupid-simple\", JamComments has no choice but to not give a rip about selling your readers garbage or collecting any bit of knowledge about them. It just collects & serves comments. Nothing fancy."
-    }
+        description: "By being \"stupid-simple\", JamComments has no choice but to not give a rip about selling your readers garbage or collecting any bit of information about them. It'll just collect & serve comments. Nothing really fancy."
+    }, 
+    {
+        heading: "Tailored to your SSG.",
+        description: "Rather than getting a big, one-size-fits-all JavaScript bundle to embed, JamComments will offer dedicated plugins specifically built for today's most popular static site generators (first GatsbyJS, followed by Eleventy, Jekyll, and maybe more)."
+    },
 ];
