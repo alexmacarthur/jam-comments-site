073aef799338f4d760d40ccfc41078d9f5d0ff77
Alex MacArthur <alex@macarthur.me>
2020-09-20 10:53:01 -0500

    try to fix it again.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     purge: {
-      enabled: process.env.NODE_ENV !== 'development',
+      enabled: process.env.NODE_ENV == 'production',
       content: ['./**/*.njk'],
       options: {
         whitelist: ['bg-red-500', 'bg-green-500'],
