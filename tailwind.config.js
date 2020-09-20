24b55c1010ca1ba6fc7555722f6d8324bcc43004
Alex MacArthur <alex@macarthur.me>
2020-09-20 00:12:26 -0500

    Adjusting tailwind settings and stuff.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    purge: [
-      './_site/**/*.html',
-    ],
+    purge: {
+      enabled: true,
+      content: ['./**/*.njk'],
+      options: {
+        whitelist: ['bg-red-500', 'bg-green-500'],
+      }
+    },
     theme: {
       extend: {},
     },
