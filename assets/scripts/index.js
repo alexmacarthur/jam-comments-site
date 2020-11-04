e1622f6bbff75b5c788dbf62ed4c8a76234d5416
Alex MacArthur <alex@macarthur.me>
2020-11-03 22:35:30 -0600

    working on mobile responsiveness.

---
+++ assets/scripts/index.js
@@ -0,0 +1,22 @@
+const MenuController = () => {
+  const nav = document.getElementById('nav');
+  const navOpen = document.getElementById('navOpen');
+  const navClose = document.getElementById('navClose');
+
+  if(!nav) {
+    return;
+  }
+
+  navOpen.addEventListener('click', (e) => {
+    console.log(nav);
+    nav.classList.add('menu-open');
+    document.body.classList.add('body-menu-open');
+  });
+
+  navClose.addEventListener('click', (e) => {
+    nav.classList.remove('menu-open');
+    document.body.classList.remove('body-menu-open');
+  });
+}
+
+MenuController();
