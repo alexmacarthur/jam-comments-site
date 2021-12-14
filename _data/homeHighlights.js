849f46a3342d65940eba90e0b74f1c7820ea5a81
Alex MacArthur <alex@macarthur.me>
2021-12-13 23:14:59 -0600

    Update the home page.

--- _data/homeHighlights.js
+++ _data/homeHighlights.js
@@ -1,14 +1,14 @@
 module.exports = [
-    {
-        heading: "Not embedded. Integrated.",
-        description: "JamComments will plug directly into your SSG's build process, spitting out statically rendered comments for your posts. Comments become an engagement-driving, performance-mindful <em>piece</em> of your site's content -- not an afterthought."
-    },
-    {
-        heading: "No ads, sketchy data mining, or tracking.",
-        description: "By being \"stupid-simple\", JamComments has no choice but to not give a rip about selling your readers garbage or collecting any bit of information about them. It'll just collect & serve comments. Nothing really fancy."
-    },
-    {
-        heading: "Tailored to your SSG.",
-        description: "Rather than getting a big, one-size-fits-all JavaScript bundle to embed, JamComments will offer dedicated plugins specifically built for today's most popular static site generators."
-    },
+  {
+    heading: "Not embedded. Integrated.",
+    description: "JamComments integrates directly into your site's build process, generating cold, hard HTML. Comments become a crawlable, SEO-friendly piece of your site's content -- not just an afterthought."
+  },
+  {
+    heading: "No ads, data mining, or tracking.",
+    description: "By being \"stupid-simple\", JamComments is too dump to give a rip about selling your readers garbage or collecting any bit of information to resell. It just collects & serve comments. Nothing really fancy."
+  },
+  {
+    heading: "Tailored to your SSG.",
+    description: "Rather than requring you to embed a bloated, one-size-fits-all JavaScript bundle, JamComments offers dedicated plugins specifically built for today's most popular static site generators."
+  },
 ];
--- _data/homeSteps.js
+++ _data/homeSteps.js
@@ -1,14 +1,14 @@
 module.exports = [
-    {
-        heading: "Create an Account",
-        description: "JamComments is FREE for a single static site and up to 10 comments per month."
-    },
-    {
-        heading: "Install & Configure the Plugin",
-        description: "Currently there's just a plugin for Gatsby. But soon enough, an easy-to-install plugin will be available for a range of static site generators."
-    }, 
-    {
-        heading: "Start Collecting Comments", 
-        description: "As new comments come in, your static site will be triggered to regenerate. So, you'll have the latest comments statically-rendered, ready to be read."
-    }
+  {
+    heading: "Create an Account",
+    description: `It's completely free. No card required. Authentication managed by <a href="https://auth0.com/" target="_blank">Auth0</a>.`
+  },
+  {
+    heading: "Install an Integration",
+    description: `Each integration comes with simple default styles that you're free to adjust as desired.`
+  },
+  {
+    heading: "Configure & Tweak",
+    description: `Create an API key, determine whether comments will be automatically approved. That's about it.`
+  },
 ];
