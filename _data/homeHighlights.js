8a566efa4254b0fc5e1775d69ab03da507ef594e
Alex MacArthur <alex@macarthur.me>
2023-11-20 23:23:28 -0600

    update copy

--- _data/homeHighlights.js
+++ _data/homeHighlights.js
@@ -1,11 +1,16 @@
 module.exports = [
   {
-    heading: "Not embedded. Integrated.",
+    heading: "Not embedded — integrated.",
     description:
       "JamComments integrates directly into your site's build process, generating cold, hard HTML. Comments become a crawlable, SEO-friendly piece of your site's content -- not an afterthought.",
   },
   {
-    heading: "No ads, data mining, or tracking.",
+    heading: "Markdown-powered editing.",
+    description:
+      "You won't find an over-featured WYSIWYG editor, JamComments supports Markdown out-of-the-box. Simple, fast, and familiar.",
+  },
+  {
+    heading: "No ads. No tracking.",
     description:
       'By being "stupid-simple", JamComments is too dumb to give a rip about selling your readers garbage or collecting any bit of information to resell. It just collects & serve comments. No more wondering how Disqus is profiting from you or your readers\' data.',
   },
@@ -13,5 +18,10 @@ module.exports = [
     heading: "Tailored to your static site generator.",
     description:
       "Rather than requring you to embed a bloated, one-size-fits-all JavaScript bundle, JamComments offers dedicated plugins specifically built for today's most popular static site generators.",
+  },
+  {
+    heading: "Authenticated or anonymous.",
+    description:
+      "By default, no account is required to comment. But if readers would like a more seamless experience, including the ability to view all previous comments, simple authentication is on the table.",
   },
 ];
