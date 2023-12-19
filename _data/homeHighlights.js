bf7ab4bb87122f35fd89c1a795a5d5f0a3197f16
Alex MacArthur <alex@macarthur.me>
2023-12-18 23:58:53 -0600

    update home page a bit

--- _data/homeHighlights.js
+++ _data/homeHighlights.js
@@ -2,26 +2,31 @@ module.exports = [
   {
     heading: "Not embedded — integrated.",
     description:
-      "JamComments integrates directly into your site's build process, generating cold, hard HTML. Comments become a crawlable, SEO-friendly piece of your site's content -- not an afterthought.",
+      "JamComments integrates directly into your site's build process, generating cold, hard HTML. Comments become a first-class piece of your site's content. Not an afterthought.",
   },
   {
     heading: "Markdown-powered editing.",
     description:
-      "You won't find an over-featured WYSIWYG editor, JamComments supports Markdown out-of-the-box. Simple, fast, and familiar.",
+      "No over-featured WYSIWYG editors. JamComments supports Markdown out-of-the-box. Simple, fast, and familiar.",
   },
   {
     heading: "No ads. No tracking.",
     description:
-      'By being "stupid-simple", JamComments is too dumb to give a rip about selling your readers garbage or collecting any bit of information to resell. It just collects & serve comments. No more wondering how Disqus is profiting from you or your readers\' data.',
+      "JamComments doens't give a rip about your readers' personal information. It just collects & serve comments. If you're interested in being a product, try Disqus.",
   },
   {
     heading: "Tailored to your static site generator.",
     description:
-      "Rather than requring you to embed a bloated, one-size-fits-all JavaScript bundle, JamComments offers dedicated plugins specifically built for today's most popular static site generators.",
+      "You won't get a bloated, one-size-fits-all JavaScript bundle. JamComments offers dedicated plugins specifically built for today's most popular static site generators.",
   },
   {
     heading: "Authenticated or anonymous.",
     description:
-      "By default, no account is required to comment. But if readers would like a more seamless experience, including the ability to view all previous comments, simple authentication is on the table.",
+      "No account is required to comment. But if readers would like a more seamless experience, including the ability to view all previous comments, simple authentication is offered.",
+  },
+  {
+    heading: "Bring your comments with you.",
+    description:
+      "No need to start from scratch. JamComments has built-in support for importing from WordPress and Disqus.",
   },
 ];
